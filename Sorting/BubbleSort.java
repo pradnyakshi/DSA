16cd0fb78a69047b124231581832c8f8e693fe30
pradnyakshi <90089158+pradnyakshi@users.noreply.github.com>
2023-07-29 17:49:14 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,33 @@
+//Bubble Sort - O(n^2)
+import java.util.*;
+
+public class BubbleSort
+{
+    public static void bubbleSort(int arr[],int n){
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main (String[]args)
+    {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt ();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+    	    arr[i] = sc.nextInt ();
+        }
+        bubbleSort(arr, n);
+        for (int i = 0; i < n; i++)
+        {
+    	    System.out.print (arr[i] + " ");
+        }
+    }
+}
