648e1d243779aaea54bc8138723396dc175c5ca1
pradnyakshi <90089158+pradnyakshi@users.noreply.github.com>
2023-07-30 17:38:24 +0530

    Update ReverseStack.java

--- Stack/ReverseStack.java
+++ Stack/ReverseStack.java
@@ -22,7 +22,6 @@ public class ReverseStack
     }
     
 	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
 		Stack<Integer> st = new Stack<>();
 		st.push(1);
 		st.push(2);
