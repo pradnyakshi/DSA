6bba429d5a06a407df3cf9b04270af149ee840d8
pradnyakshi <90089158+pradnyakshi@users.noreply.github.com>
2023-07-30 11:21:57 +0530

    Create CountSort.java

---
+++ Sorting/CountSort.java
@@ -0,0 +1,39 @@
+//Count Sort - O(n+k)
+
+import java.util.*;
+
+public class CountSort{
+    public static void countSort(int arr[],int n){
+        int k = arr[0];
+        for(int i=1;i<n;i++){
+            k = Math.max(k,arr[i]);
+        }
+        int count[] = new int[10];
+        for(int i=0;i<10;i++){
+            count[i] = 0;
+        }
+        for(int i=0;i<n;i++){
+            count[arr[i]]++;
+        }
+        for(int i=1;i<=k;i++){
+            count[i]+=count[i-1];
+        }
+        int output[] = new int[n];
+        for(int i=n-1;i>=0;i--){
+            output[--count[arr[i]]] = arr[i];
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(output[i] + " ");
+        }
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        countSort(arr,n);
+    }
+}
